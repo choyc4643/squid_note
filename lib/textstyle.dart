52a5602c980b677a984a483167c7c50cac46737a
choyc4643 <choyc4643@naver.com>
2022-03-28 18:58:30 +0900

    textstyle.dart 생성

---
+++ lib/textstyle.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+//heading
+TextStyle headingstyle({double? size, Color? color}) {
+  return TextStyle(
+    fontSize: size,
+    fontFamily: 'Bold',
+    color: color,
+  );
+}
+
+
+TextStyle subtitlestyle({double? size, Color? color}) {
+  return TextStyle(
+    fontSize: size,
+    fontFamily: 'Regular',
+    color: color,
+  );
+}
